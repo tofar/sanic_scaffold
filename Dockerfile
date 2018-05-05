add56ff58a2c5c16d948407b1b9ee182f6783553
tofar <yun_tofar@qq.com>
2018-05-05 19:21:03 +0800

    change the WEB_ENV to PORT

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM python:3.6
-LABEL maintainer="yun_tofar@qq.com"
-LABEL version="1.0"
-LABEL description="python sanic scaffold"
-
+LABEL maintainer="yun_tofar@qq.com" \
+    version="1.0"  \
+    description="python sanic scaffold"
 COPY ./src /app
 
 WORKDIR /app
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     debug = True
     if os.environ.get("WEB_ENV", "dev") == "prod":
         debug = False
-    port = int(os.environ.get("WEB_ENV", 5678))
+    port = int(os.environ.get("PORT", 5678))
     host = os.environ.get("HOST", "0.0.0.0")
 
     app.run(host=host, port=port, debug=debug)
